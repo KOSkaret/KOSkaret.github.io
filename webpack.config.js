f5226b1449a691247bbcafa2e8469396ca6530f2
Knut Olav <tyfonknut@gamil.com>
2017-06-25 13:31:49 +0200

    Added HTML, CSS and JS code.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'main.css'
+});
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: extractPlugin.extract({
+          use: ['css-loader']
+        })
+      }, {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use:['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.html'
+    })
+  ]
+}
