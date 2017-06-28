f1269fe53ac9ad3ea6d39d2445aff1e6e22533ff
Knut Olav <tyfonknut@gamil.com>
2017-06-28 20:38:21 +0200

    Experimented on flexbox, tried to make the first section cover majority of the screen when opened, and experimented with other stuff.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
 import '../sass/main.scss';
 
+var w = window;
+var d = document;
+var e = d.documentElement;
+var b = d.body;
 
-var section1 = document.getElementById('section1');
+var section1 = d.getElementById('section1');
+
+var y = w.innerHeight || e.clientHeight || b.clientHeight;
+
+
+function test(){
+
+  section1.style.height = y + 'px';
+
+};
+
+window.onload = test;
