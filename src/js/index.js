dcd525e1debe510268d66381f9af17109d88f6ea
Knut Olav <tyfonknut@gamil.com>
2017-07-02 13:46:03 +0200

    Deleted, since it was distracting to workflow for what I want to achieve. And overcomplicated the progress.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import '../sass/main.scss';
 
+/*
 var w = window;
 var d = document;
 var e = d.documentElement;
 var b = d.body;
 
 var section1 = d.getElementById('section1');
+var section2 = d.getElementById('section2');
+var section3 = d.getElementById('section3');
 
 var y = w.innerHeight || e.clientHeight || b.clientHeight;
 
@@ -13,7 +16,10 @@ var y = w.innerHeight || e.clientHeight || b.clientHeight;
 function test(){
 
   section1.style.height = y + 'px';
+  section2.style.height = y + 'px';
+  section3.style.height = y + 'px';
 
 };
 
 window.onload = test;
+*/
