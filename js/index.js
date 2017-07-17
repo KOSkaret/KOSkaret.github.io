700c0cbe0f825f5daf6540e6cc41d5633a9eb734
Knut Olav <tyfonknut@gamil.com>
2017-07-17 08:49:12 +0200

    Changed jumbotron, added an example code to fix jumping with the nav. Not a complete fix, byt did well for desktop.

--- js/index.js
+++ js/index.js
@@ -10,3 +10,7 @@ function closeSlideMenu(){
   sideMenu.style.width= '0px';
   content.style.marginLeft= '0px';
 }
+
+var shiftWindow = function() { scrollBy(0, -20) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
